4bd2552b31a176866d38694fe95c16d3e9a9ef4a
Dorian Weidler <dorian.weidler@novatec-gmbh.de>
2019-11-19 12:10:38 +0100

    Enabled CORS on lab1 resourceserver

--- lab1/library-server-complete-automatic/src/main/java/com/example/library/server/config/WebSecurityConfiguration.java
+++ lab1/library-server-complete-automatic/src/main/java/com/example/library/server/config/WebSecurityConfiguration.java
@@ -1,20 +1,23 @@
 package com.example.library.server.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
+        .and().cors().and()
         .csrf()
         .disable()
         .authorizeRequests()
@@ -23,5 +26,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .oauth2ResourceServer()
         .jwt();
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
   }
 }
