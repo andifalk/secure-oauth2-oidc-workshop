c92ed05753b9f759a3c50cbd0fc3993f47f49d6f
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-01-25 00:38:03 +0100

    update keycloak scripts and added windows support

---
+++ setup_keycloak/export_keycloak_realm.bat
@@ -0,0 +1,10 @@
+@echo off
+
+setlocal
+
+# replace this with your own installation directory of keycloak
+set KEYCLOAK_HOME=C:\keycloak-8.0.1
+
+%KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.migration.action=export \
+-Dkeycloak.migration.realmName=workshop -Dkeycloak.migration.provider=singleFile \
+-Dkeycloak.migration.file=keycloak_realm_workshop.json
