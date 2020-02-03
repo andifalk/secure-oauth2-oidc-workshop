214bacab670a086598e8b0cb285f2b956d560e8d
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-02-03 20:22:33 +0100

    fixed keycloak setup scripts for windows

--- setup/export_keycloak_realm.bat
+++ setup/export_keycloak_realm.bat
@@ -5,6 +5,4 @@ setlocal
 # replace this with your own installation directory of keycloak
 set KEYCLOAK_HOME=C:\keycloak-8.0.1
 
-%KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.migration.action=export \
--Dkeycloak.migration.realmName=workshop -Dkeycloak.migration.provider=singleFile \
--Dkeycloak.migration.file=keycloak_realm_workshop.json
+%KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.migration.action=export -Dkeycloak.migration.realmName=workshop -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=keycloak_realm_workshop.json
--- setup/import_keycloak_realm.bat
+++ setup/import_keycloak_realm.bat
@@ -5,7 +5,4 @@ setlocal
 # replace this with your own installation directory of keycloak
 set KEYCLOAK_HOME=C:\keycloak-8.0.1
 
-%KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.migration.action=import \
--Dkeycloak.migration.realmName=workshop \
--Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=keycloak_realm_workshop.json \
--Dkeycloak.migration.strategy=OVERWRITE_EXISTING
+%KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.migration.action=import -Dkeycloak.migration.realmName=workshop -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=keycloak_realm_workshop.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
