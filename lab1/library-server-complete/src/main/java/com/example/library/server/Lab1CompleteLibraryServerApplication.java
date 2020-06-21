777bf59b335124acb701cedd24bc404173558f6f
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-06-21 22:08:30 +0200

    rename complete app of Lab1

--- lab1/library-server-complete/src/main/java/com/example/library/server/Lab1CompleteLibraryServerApplication.java
+++ lab1/library-server-complete/src/main/java/com/example/library/server/Lab1CompleteLibraryServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab1CompleteCustomLibraryServerApplication {
+public class Lab1CompleteLibraryServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Lab1CompleteCustomLibraryServerApplication.class, args);
+    SpringApplication.run(Lab1CompleteLibraryServerApplication.class, args);
   }
 }
