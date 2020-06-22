7306714d6f202c5a17c48d6d9977382e48c901cc
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-06-22 12:18:06 +0200

    explicitly set H2 for keycloak docker container script

--- setup/run_keycloak_docker.bat
+++ setup/run_keycloak_docker.bat
@@ -5,4 +5,4 @@ setlocal
 # replace this config with your own directory
 set WORKSHOP_HOME=C:\secure-oauth2-oidc-workshop\setup
 
-docker run --rm -p 8080:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_IMPORT=/tmp/keycloak_realm_workshop.json -v %WORKSHOP_HOME%\keycloak_realm_workshop.json:/tmp/keycloak_realm_workshop.json jboss/keycloak:10.0.2
+docker run --rm -p 8080:8080 -e DB_VENDOR=h2 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_IMPORT=/tmp/keycloak_realm_workshop.json -v %WORKSHOP_HOME%\keycloak_realm_workshop.json:/tmp/keycloak_realm_workshop.json jboss/keycloak:10.0.2
