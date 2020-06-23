1767ab2160513df0f12baba03c2f1ca17c16b7db
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-06-23 17:05:05 +0200

    fix windows batch files

--- setup/export_keycloak_realm.bat
+++ setup/export_keycloak_realm.bat
@@ -2,7 +2,7 @@
 
 setlocal
 
-# replace this with your own installation directory of keycloak
+rem replace this with your own installation directory of keycloak
 set KEYCLOAK_HOME=C:\keycloak-10.0.1
 
 %KEYCLOAK_HOME%\bin\standalone.bat -Dkeycloak.migration.action=export -Dkeycloak.migration.realmName=workshop -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=keycloak_realm_workshop.json
