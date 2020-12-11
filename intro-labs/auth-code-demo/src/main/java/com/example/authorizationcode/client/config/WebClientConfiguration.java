0f1efff42af9868982bebba9184e741c546a5f93
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-12-11 19:11:11 +0100

    bump spring boot 2.4.1, gradle 3.7.1 version and use java 11

--- intro-labs/auth-code-demo/src/main/java/com/example/authorizationcode/client/config/WebClientConfiguration.java
+++ intro-labs/auth-code-demo/src/main/java/com/example/authorizationcode/client/config/WebClientConfiguration.java
@@ -2,28 +2,13 @@ package com.example.authorizationcode.client.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.ProxyProvider;
 
 @Configuration
 public class WebClientConfiguration {
 
-  @Profile("!proxy")
   @Bean
   WebClient webClient() {
     return WebClient.builder().build();
-  }
-
-  @Profile("proxy")
-  @Bean
-  WebClient webClientWithProxy() {
-    HttpClient httpClient = HttpClient.create().followRedirect(false)
-            .tcpConfiguration(tcpClient ->
-                    tcpClient.proxy(proxy -> proxy.type(ProxyProvider.Proxy.HTTP).host("localhost").port(8085)));
-    ReactorClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
-    return WebClient.builder().clientConnector(connector).build();
   }
 }
